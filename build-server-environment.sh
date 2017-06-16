a107f66ffd6cd9ea3342693720ae3a1d298f8c88
daton <tonydangelo123@gmail.com>
2017-06-16 11:17:01 +0200

    update version

--- build-server-environment.sh
+++ build-server-environment.sh
@@ -18,24 +18,24 @@ sudo apt-get upgrade -y
 #install curl
 sudo apt-get install -y curl
 #install nodejs 
-sudo curl -sL https://deb.nodesource.com/setup_6.x | sudo bash -
+sudo curl -sL https://deb.nodesource.com/setup_7.x | sudo bash -
 sudo apt-get install -y nodejs
 
 # import public key 
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
 # Create a /etc/apt/sources.list.d/mongodb-org-3.2.list file for MongoDB
-echo "deb http://repo.mongodb.org/apt/debian wheezy/mongodb-org/3.2 main" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.2.list
+echo "deb http://repo.mongodb.org/apt/debian wheezy/mongodb-org/3.4.5 main" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.4.5.list
 # Reload local package database
 sudo apt-get update
 # Install the MongoDB packages
 sudo apt-get install -y mongodb-org
 # Start MongoDB
-sudo service mongod start
+sudo systemctl start mongod
+# Enable startup MongoDb on boot
+sudo systemctl enable mongod
 
 # Install Git (it's required for bower)
 sudo apt-get install -y git-core
-git config --global user.name "Tony D'Angelo'"
-git config --global user.email tonydangelo123@gmail.com
 
 # Install Bower and pm2
-sudo npm i -g bower pm2
+sudo npm i -g pm2
