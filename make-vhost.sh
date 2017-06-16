b908423fd26fbf6da353c7b8d9576dbba570da45
daton <tonydangelo123@gmail.com>
2017-06-16 10:49:33 +0200

    add script make-vhost

---
+++ make-vhost.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+START=$(date +"%s")
+DATE=$(date +"%d-%m-%Y_%H-%M")
+TODAY=$(date)
+
+printf "\nNow is $TODAY \n\n"
+
+
+if [ -n "$1" ]; then
+figlet "Yooo Tony!"
+else
+echo "### --- ### --- vhost name required! Exiting... --- ### --- ###"
+exit 1
+fi
+
+echo "------------- Building of new vhost: $1 started! --------------"
